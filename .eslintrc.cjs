f8de7514dd18d17a2ac7a80185c5aca7128e966c
lunabar <1434277075@qq.com>
2024-07-20 22:01:30 +0800

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    'vue/multi-word-component-names': 0
+    // 不再强制组件命需要用驼峰命名
+  }
+}
---
+++ src/components/Button/Button.vue
@@ -0,0 +1,34 @@
+<template>
+  <button
+    class="vk-Button"
+    :class = "{
+      [`vk-button-${type}`]: type,
+      [`vk-button-${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-disabled': disabled,
+    }"
+    :disabled="disabled"
+  >
+    <span>
+      <slot/>
+    </span>
+  </button>
+
+</template>
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  export default defineComponent({
+    name: 'VButton'
+  })
+</script>
+
+<script setup lang="ts">
+  // import { buttonProps } from './types'; 
+  import type { ButtonProps } from './types';
+  // defineProps(buttonProps)
+  defineProps<ButtonProps>()
+</script>
+
+<style>
+</style>
