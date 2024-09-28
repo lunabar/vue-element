fee8380895a039c0ae1ec0678a9721c343a2c584
lunabar <1434277075@qq.com>
2024-09-28 22:09:23 +0800

    修改

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -1,45 +1,37 @@
 <template>
-  <button
-    ref="_ref"
-    class="vk-button"
-    :class = "{
-      [`vk-button--${type}`]: type,
-      [`vk-button--${size}`]: size,
-      'is-plain': plain,
-      'is-round': round,
-      'is-circle': circle,
-      'is-disabled': disabled,
-    }"
-    :disabled="disabled"
-    :type="nativeType"
-    :autofocus="autofocus"
-  >
+  <button ref="_ref" class="vk-button" :class="{
+    [`vk-button--${type}`]: type,
+    [`vk-button--${size}`]: size,
+    'is-plain': plain,
+    'is-round': round,
+    'is-circle': circle,
+    'is-disabled': disabled,
+  }" :disabled="disabled" :type="nativeType" :autofocus="autofocus">
     <span>
-      <slot/>
+      <slot />
     </span>
   </button>
 
 </template>
 
 <script setup lang="ts">
-  // import { buttonProps } from './types'; 
-  import type { ButtonProps } from './types';
-  import { ref } from 'vue';
-  // defineProps(buttonProps)
-  defineOptions({
-    name: 'VButton', 
-  })
-  withDefaults(defineProps<ButtonProps>(),{
-    nativeType: 'button',
-  })
-  const _ref = ref<HTMLButtonElement>()
-  defineExpose({
-    ref: _ref,
-  })
+// import { buttonProps } from './types'; 
+import type { ButtonProps } from './types';
+import { ref } from 'vue';
+// defineProps(buttonProps)
+defineOptions({
+  name: 'VButton',
+})
+withDefaults(defineProps<ButtonProps>(), {
+  nativeType: 'button',
+})
+const _ref = ref<HTMLButtonElement>()
+defineExpose({
+  ref: _ref,
+})
 
 
 
 </script>
 
-<style>
-</style>
+<style></style>
